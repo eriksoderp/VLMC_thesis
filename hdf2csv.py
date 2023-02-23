439f507402d0a597f22e654362cfc3a6d714eef6
eriksoderp <54850702+eriksoderp@users.noreply.github.com>
2023-02-23 09:45:25 +0100

    Create hdf2csv.py
    Script to convert VLMC distances in .hf format to .csv format
     
    it takes hf_file_name.hf, converts it to csv and puts the results in csv_file_name.csv
    
    command is "python3 hdf2csv.py hf_file_name.hf > csv_file_name.csv"

---
+++ hdf2csv.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import sys
+import numpy as np
+import h5py
+
+hf_path = sys.argv[1]
+
+df = pd.DataFrame(np.array(h5py.File(hf_path)['distances']['dvstar-0']))
+
+df.to_csv(sys.stdout, index=False)
