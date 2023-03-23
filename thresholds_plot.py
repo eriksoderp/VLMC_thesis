7039fd64837bc1bfa305e397deb4aaa177f128cb
eriksoderp <54850702+eriksoderp@users.noreply.github.com>
2023-03-23 14:08:12 +0100

    Update thresholds_plot.py

--- thresholds_plot.py
+++ thresholds_plot.py
@@ -4,33 +4,15 @@ import seaborn as sns
 import sys
 import matplotlib.pyplot as plt
 
-evo_path = sys.argv[1]
-df_evo = pd.read_csv(evo_path)
+df = pd.read_csv("vlmc_distances.csv")
 
-vlmc_path = sys.argv[2]
-
-df = pd.DataFrame()
-
-evo_series = pd.Series(dtype='float64')
-evo_series = df_evo['0']
-
-extension = '.csv'
-temp_df = pd.DataFrame()
-temp_df['Evolutionary distance'] = evo_series
-for threshold, s_threshold in [(0.0,'00'), (0.5,'05'), (1.2,'12'), (3.9075,'39075')]:
-    df_vlmc = pd.read_csv(vlmc_path + '_' + s_threshold + extension)
-    vlmc_series = pd.Series(dtype='float64')
-    vlmc_series = df_vlmc['0']
-    temp_df['VLMC distance'] = vlmc_series
-    temp_df['Threshold'] = threshold
-
-    df = pd.concat([df, temp_df])
+print(df)
 
 sns.set_theme()
 
 sns.relplot(
     data=df,
-    x='VLMC distance', y='Evolutionary distance', hue='Threshold'
+    x='VLMC dist', y='Evolutionary dist', hue=df[['threshold', 'min_count', 'max_depth']].apply(tuple, axis=1)
 )
 
 plt.show()
