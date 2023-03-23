71e867ff6adbebd31995004d8735a4f535e4d61b
eriksoderp <54850702+eriksoderp@users.noreply.github.com>
2023-03-23 14:07:05 +0100

    Update dvstar_build.py

--- dvstar_build.py
+++ dvstar_build.py
@@ -8,15 +8,21 @@ import os
 from enum import Enum
 import numpy as np  
 import multiprocessing
-#from tqdm import tqdm
+import tempfile
+from tqdm import tqdm
+import re
 
 def dvstar_build(genome_path: Path, out_path: Path, threshold: float, min_count: int, max_depth: int):
     opath = ""
     out_path = out_path / Path(str(out_path) + f"_{threshold}_{min_count}_{max_depth}")
     out_path.mkdir(exist_ok=True)
+
     desc = str(threshold) + '_' + str(min_count) + '_' + str(max_depth)
-    for genome in os.listdir(genome_path): #tqdm(os.listdir(genome_path), desc=desc, leave=False, position=1): #tqdm for development - useless on Bayes
-        opath = out_path / get_bintree_name(genome, threshold, min_count, max_depth)
+    for genome in tqdm(os.listdir(genome_path), desc=desc): #tqdm for development - useless on Bayes ,os.listdir(genome_path): #
+        if "_1." in str(genome):
+            opath = out_path / Path("original")
+        else:
+            opath = out_path / get_bintree_name(genome, threshold, min_count, max_depth)
         
         args = (
             "./build/dvstar",
@@ -31,9 +37,12 @@ def dvstar_build(genome_path: Path, out_path: Path, threshold: float, min_count:
             "--fasta-path",
             genome_path / genome,
             "--out-path",
-            opath
+            opath,
+            "--in-or-out-of-core",
+            "external"
         )
         subprocess.run(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)  
+
 
 def get_bintree_name(genome_path: str, threshold: float, min_count: int, max_depth: int):
     return os.path.splitext(genome_path)[0] + f"_{threshold}_{min_count}_{max_depth}.bintree"
@@ -41,9 +50,12 @@ def get_bintree_name(genome_path: str, threshold: float, min_count: int, max_dep
 def compare_trees(genome_path, out_path, threshold, min_count, max_depth):
     out_path = out_path / Path(str(out_path) + f"_{threshold}_{min_count}_{max_depth}")
     trees = os.listdir(out_path)
-    original = trees[0]
     distances = []
     sizes = []
+    trees.sort(key=lambda var:[int(x) if x.isdigit() else x for x in re.findall(r'[^0-9]|[0-9]+', var)])
+
+    original = trees[0]
+    print(trees)
 
     for i, tree in enumerate(trees[1:]):
         args = (
@@ -55,11 +67,8 @@ def compare_trees(genome_path, out_path, threshold, min_count, max_depth):
             "--to-path",
             out_path / tree
         )
-        if tree == original:
-            distances.append(0.0)
-        else:
-            proc = subprocess.Popen(args, stdout=subprocess.PIPE)
-            distances.append((i+1, float(proc.stdout.readlines()[-1])))
+        proc = subprocess.Popen(args, stdout=subprocess.PIPE)
+        distances.append((i+1, float(proc.stdout.readlines()[-1])))
 
     for tree in trees:
         args = (
@@ -83,11 +92,13 @@ def build(argv):
 
     combinations = [(genome_path, out_path,) + (threshold, min_count, max_depth) for threshold in (0, 0.5, 1.2, 3.9075) for min_count in (2, 10, 25, 100) for max_depth in (9, 12)]
 
-    for g, o, t, m, d in combinations: #tqdm(combinations, position=0, desc='outer loop', leave=False): #tqdm for development - useless on Bayes
-        dvstar_build(g, o, t, m, d)
+    pool_obj1 = multiprocessing.Pool(number_of_cores)
+    pool_obj1.starmap(dvstar_build, combinations)
+    pool_obj1.close()
     
     pool_obj = multiprocessing.Pool(number_of_cores)
     ans = pool_obj.starmap(compare_trees, combinations)
+    pool_obj.close()
 
     columns = ['Sequence', 'VLMC dist', 'Original VLMC size', 'Mod VLMC size', 'threshold', 'min_count', 'max_depth']
     df = pd.DataFrame(columns=list(columns))
