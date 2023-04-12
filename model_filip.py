38fcf70668253d44e92b87d63cd8074b75fad922
HelmrothF <helmroth@student.chalmers.se>
2023-04-12 10:45:32 +0200

    uppdaterad modell med filnamn

--- model_filip.py
+++ model_filip.py
@@ -9,15 +9,15 @@ from scipy.stats import spearmanr
 import pickle
 import sys
 import matplotlib.pyplot as plt
-from matplotlib import rc, rcParams
-import numpy as np
+from matplotlib import rc
+import os
 
 
 def make_model(argv):
     csv_path = argv[0]
     number_of_cores = int(argv[1])
     number_of_kfolds = int(argv[2])
-    model_filename = argv[3]
+    dataset_name = str(argv[3])
     X_args = argv[4].split(',')
 
     print(X_args)
@@ -25,22 +25,25 @@ def make_model(argv):
     # Load the .csv file into a DataFrame
     data = pd.read_csv(csv_path)
 
+    ### ÄNDRA DESSA
+    max_depth_values = [9, 12]
+    min_count_values = [25, 100]
+    threshold_values = [3.9075]
+    hidden_layer_sizes_values = [(1,2),(3,3,3)]
+    
     # Filter the rows
-    data = data[data['max_depth'].isin([12])]
-    data = data[data['min_count'].isin([100])]
-    data = data[data['threshold'].isin([3.9075])]
+    data = data[data['max_depth'].isin(max_depth_values)]
+    data = data[data['min_count'].isin(min_count_values)]
+    data = data[data['threshold'].isin(threshold_values)]
 
-    # Prepare the data
-    # ["VLMC dist", "threshold", "min_count", "max_depth"]
     X = data[X_args].fillna(0)
     y = data["evo dist"]
-    
+
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Normalize the features using a Scaler
     scaler = MinMaxScaler()
-    #scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
     
@@ -50,20 +53,12 @@ def make_model(argv):
     y_test_scaled = scaler_y.transform(y_test.values.reshape(-1, 1)).flatten()
 
     parameter_space = {
-        'hidden_layer_sizes': [(1500,1250,1000,750),(2000,1500,1000)],
+        'hidden_layer_sizes': hidden_layer_sizes_values,
         'activation': ['relu'],
         'solver': ['adam'],
         #'alpha': [0.0001, 0.01],
         #'learning_rate': ['constant','adaptive'],
     }
-
-    """parameter_space = {
-        'hidden_layer_sizes': [(200,200,200), (500,500,500), (1000,1000,1000)],
-        'activation': ['tanh', 'relu'],
-        'solver': ['sgd', 'adam'],
-        'alpha': [0.0001, 0.01],
-        'learning_rate': ['constant','adaptive'],
-    }"""
 
     # Train and print MLP Regression
     mlp_regressor = MLPRegressor()
@@ -71,7 +66,8 @@ def make_model(argv):
         mlp_regressor, 
         parameter_space, 
         n_jobs=number_of_cores, 
-        cv=number_of_kfolds
+        cv=number_of_kfolds,
+        verbose=3
     )
     
     grid_search.fit(X_train_scaled, y_train_scaled)
@@ -104,7 +100,13 @@ def make_model(argv):
     print("RMSE      :", round(mean_squared_error(y_test, y_pred_lr),4), "\n")
 
     # Save the model
-    pickle.dump(best_mlp, open(model_filename, 'wb'))
+    if not os.path.exists('./models'):
+        os.makedirs('./models')
+
+    best_hidden_layer_sizes = grid_search.best_params_['hidden_layer_sizes']
+    data_file_name = f"./models/model_{str(dataset_name)}_layers_{str(best_hidden_layer_sizes)}_max-depth_{str(max_depth_values)}_min-count_{str(min_count_values)}_threshold_{str(threshold_values)}_cv_{str(number_of_kfolds)}.png"
+
+    pickle.dump(best_mlp, open(data_file_name, 'wb'))
 
     if(1):
 
@@ -159,12 +161,16 @@ def make_model(argv):
         ax2.set_axisbelow(True)
         ax2.grid(color='gray', linestyle='dashed', linewidth=0.5)
 
-        plt.savefig("fig.png"))
+        if not os.path.exists('./plots'):
+            os.makedirs('./plots')
+
+        plot_file_name = f"./plots/plot_{str(dataset_name)}_layers_{str(best_hidden_layer_sizes)}_max-depth_{str(max_depth_values)}_min-count_{str(min_count_values)}_threshold_{str(threshold_values)}_cv_{str(number_of_kfolds)}.png"
+        plt.savefig(plot_file_name)
         #plt.show()
 
 
 if __name__ == "__main__":
     if len(sys.argv[1:]) != 5:
-        print("The script requires 5 arguments as <file.csv> <number_of_cores> <number_of_kfolds> <model_filename> <list_of_included_features>")
+        print("The script requires 5 arguments as <file.csv> <number_of_cores> <number_of_kfolds> <dataset_name> <list_of_included_features>")
     else:
         make_model(sys.argv[1:])
